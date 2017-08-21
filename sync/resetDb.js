a588b59b787e2f0e93c7084825b6cc651a570caf
Владимир Чапурин <vladimir.chapurin@simbirsoft.com>
2017-08-21 16:33:45 +0400

    Выставил правильную последовательность создания таблиц в бд

--- sync/resetDb.js
+++ sync/resetDb.js
@@ -1,31 +1,34 @@
 const models = require('../server/models');
 
+const models = require('../server/models');
+
 const Models  = [
-/*  models.ProjectRolesDictionary,
-  models.ProjectStatusesDictionary,
-  models.SprintStatusesDictionary,
-  models.TaskStatusesDictionary,
-  
-  models.ProjectUsers,
-  models.ProjectAttachments,
-  models.TaskAttachments,
-  
-  models.Portfolio,
-  models.Project,
-  models.Sprint,
-  models.Task,
-  models.User,
-  models.Department,
-  models.UserDepartments,
-  models.Token,*/
-  
+  // models.ProjectRolesDictionary,
+  // models.ProjectStatusesDictionary,
+  // models.SprintStatusesDictionary,
+  // models.TaskStatusesDictionary,
+  // models.TaskTypesDictionary,
+  // models.ProjectRolesDictionary,
+  //
+  // models.Portfolio,
+  // models.Project,
+  // models.Sprint,
+  // models.User,
+  // models.Task,
+  // models.Department,
+  // models.UserDepartments,
+  // models.Token,
+  //
+  // models.ProjectUsers,
+  // models.ProjectAttachments,
+  // models.TaskAttachments,
+  //
   // models.TimesheetTypesDictionary,
   // models.Timesheet,
   // models.ModelHistory,
   // models.TaskTasks,
-  models.ItemTag,
+  // models.ItemTag,
   // models.Tag,
-  //models.ProjectRolesDictionary,
 ];
 
 const dictionariesModels  = [
@@ -33,9 +36,36 @@ const dictionariesModels  = [
   // models.SprintStatusesDictionary,
   // models.TaskStatusesDictionary,
   // models.TaskTypesDictionary,
-  //models.ProjectRolesDictionary,
+  // models.ProjectRolesDictionary,
   // models.TimesheetTypesDictionary,
 ];
+
+
+(() => {
+  let chain = Promise.resolve();
+  Models.forEach(function(Model) {
+    chain = chain
+      .then(() => Model.sync({force: true}));
+  });
+
+
+  dictionariesModels.forEach(function(model) {
+    chain = chain
+      .then(() => {
+        return model.destroy({where: {}})
+          .then(() => model.bulkCreate(model.values));
+      });
+  });
+
+
+  chain
+    .then(() => {
+      console.log('Done');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+})();
 
 
 (() => {
