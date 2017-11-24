5767b8daf555e1dcf68a7116babe0ad2123b2662
Владимир Чапурин <vladimir.chapurin@simbirsoft.com>
2017-11-24 17:39:54 +0400

    feature/ST-389/create-delete-drafts-by-cron-job

--- cronjobs/createDrafts/index.js
+++ cronjobs/createDrafts/index.js
@@ -2,11 +2,11 @@ const moment = require('moment');
 const createDraftsService = require('./service');
 
 (async function createDrafts () {
+  const onDate = moment().format('YYYY-MM-DD');
   try {
-    const onDate = moment().format('YYYY-MM-DD');
     await createDraftsService().call(onDate);
-    console.info('Done!');
   } catch (e) {
     console.error(e);
   }
+  console.info('Done!');
 }());
--- cronjobs/createDrafts/service/index.js
+++ cronjobs/createDrafts/service/index.js
@@ -1,20 +1,25 @@
 const createError = require('http-errors');
 const models = require('../../../server/models');
-const { createTaskDrafts } = require('./taskDrafts');
-const { createProjectDrafts } = require('./projectDrafts');
-const { createNoProjectDrafts } = require('./noProjectDrafts');
+const request = require('./request');
 
 module.exports = () => {
+  function getEntities (onDate, tasks) {
+    return tasks.map(task => ({
+      onDate,
+      userId: task.performerId,
+      typeId: models.TimesheetTypesDictionary.IMPLEMENTATION
+    }));
+  }
+
   return {
     call: async (onDate) => {
       let transaction;
       try {
+        const tasks = await models.Task.findAll(request(onDate));
+        const entities = getEntities(onDate, tasks);
         transaction = await models.sequelize.transaction();
-        await createTaskDrafts(onDate, transaction);
-        await createProjectDrafts(onDate, transaction);
-        await createNoProjectDrafts(onDate, transaction);
+        await models.TimesheetDraft.bulkCreate(entities, { transaction });
         await transaction.commit();
-
       } catch (e) {
         transaction.rollback();
         throw createError(e);
