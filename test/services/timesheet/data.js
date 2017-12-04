58dad1017d737dd51f43e1496248db351bb09fb9
e.lukin <e.lukin@simcase.ru>
2017-12-04 12:37:44 +0400

    fix test for timesheet service

--- test/services/timesheet/data.js
+++ test/services/timesheet/data.js
@@ -1,14 +1,19 @@
+/* eslint no-unused-expressions: 0 */
+
 const chai = require('chai');
+const mocha = require('mocha');
 const expect = chai.expect;
 const moment = require('moment');
-const TimesheetController = require('./../../../server/controllers/api/v2/TimesheetController');
+const { getDateRange } = require('./../../../server/services/timesheets/getTracksAll');
+const describe = mocha.describe;
+const it = mocha.it;
 
 describe('Timesheet Service', () => {
   describe('Date range generator', () => {
     it('should return error after invalid data range', () => {
       const startDay = moment('2017-11-23');
       const endDay = moment('2017-11-23');
-      const getInvalidRange = () => TimesheetController.getDateRange(startDay, endDay);
+      const getInvalidRange = () => getDateRange(startDay, endDay);
 
       expect(getInvalidRange).to.throw;
     });
@@ -16,7 +21,7 @@ describe('Timesheet Service', () => {
     it('should return correct data range', () => {
       const startDay = moment('2017-11-21');
       const endDay = moment('2017-11-23');
-      const range = TimesheetController.getDateRange(startDay, endDay);
+      const range = getDateRange(startDay, endDay);
       const expectedRange = [ '2017-11-23', '2017-11-22', '2017-11-21' ];
 
       expect(expectedRange).to.deep.eql(range);
