089a2e6f5ed6c1d7a03e3af5113c5fb7f580a861
Владимир Чапурин <vladimir.chapurin@simbirsoft.com>
2017-12-19 16:00:15 +0400

    [ST-528] Changed response Sequelize.ValidationError

--- server/middlewares/ErrorHandlerMiddleWare.js
+++ server/middlewares/ErrorHandlerMiddleWare.js
@@ -3,48 +3,51 @@ const Sequelize = require('sequelize');
 const ldap = require('ldapjs');
 const production = process.env.NODE_ENV === 'production';
 
-module.exports = function(err, req, res, next) {
-  
+module.exports = function (err, req, res, next) {
+
   if (err.mapped) { // expressValidator
     err.status = 400;
     err.name = 'ValidationError';
     err.message = {errors: err.array()};
   }
-  
-  if(err instanceof ldap.InvalidCredentialsError) {
+
+  if (err instanceof ldap.InvalidCredentialsError) {
     err.status = 404;
   }
-  
-  if(err instanceof Sequelize.ForeignKeyConstraintError) {
+
+  if (err instanceof Sequelize.ForeignKeyConstraintError) {
     err.status = 400;
   }
-  
-  // if(err instanceof Sequelize.Error) {
-  //   err.status = 400;
-  // }
-  
-  if(err instanceof Sequelize.ValidationError) {
+
+  if (err instanceof Sequelize.ValidationError) {
     err.status = 400;
     err.name = 'ValidationError';
-    err.message = err.errors.map((el) => { return el.path + ' ( ' + el.message + ')'; }).join('. ');
+    err.message = {
+      errors: err.errors.map(e => ({
+        value: e.value,
+        msg: e.message,
+        type: e.type,
+        param: e.path
+      }))
+    };
   }
-  
+
   sendError(res, err);
 };
 
 
-function sendError(res, err) {
+function sendError (res, err) {
   let status = err.status || err.statusCode || 500;
   if (status < 400) status = 500;
   res.statusCode = status;
-  
-  let body = {
+
+  const body = {
     status: status
   };
-  
+
   // show the stacktrace when not in production
   if (!production) body.stack = err.stack;
-  
+
   // internal server errors
   if (status >= 500) {
     console.error(err.stack);
@@ -52,13 +55,13 @@ function sendError(res, err) {
     res.json(body);
     return;
   }
-  
+
   // client errors
   body.message = err.message;
-  
+
   if (err.code) body.code = err.code;
   if (err.name) body.name = err.name;
   if (err.type) body.type = err.type;
-  
+
   res.json(body);
 }
