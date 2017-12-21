e2d820728b9181144f0081ccf4652c0428148e99
Squall <j.skvoll@gmail.com>
2017-12-21 17:52:08 +0400

    send task spend

--- server/services/timesheets/index.js
+++ server/services/timesheets/index.js
@@ -4,6 +4,7 @@ const { createDraft } = require('./createDraft');
 const { getDraft } = require('./createDraft');
 const { destroy } = require('./destroy');
 const { getTracksAll } = require('./getTracksAll');
+const { getTaskSpent } = require('./spent');
 const { update } = require('./update');
 const { updateDraft } = require('./updateDraft');
 const { isNeedCreateDraft } = require('./utils');
@@ -12,6 +13,7 @@ module.exports = {
   create,
   createDraft,
   destroy,
+  getTaskSpent,
   getTracksAll,
   list,
   update,
