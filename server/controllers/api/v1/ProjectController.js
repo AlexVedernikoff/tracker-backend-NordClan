4f39b490f2a76cc661984f8fbc023f640f88207e
Владимир Чапурин <vladimir.chapurin@simbirsoft.com>
2017-12-27 17:32:09 +0400

    [ST-757] added more fields in responses with sprint

--- server/controllers/api/v1/ProjectController.js
+++ server/controllers/api/v1/ProjectController.js
@@ -88,7 +88,8 @@ exports.read = function (req, res, next){
         {
           as: 'sprints',
           model: Sprint,
-          attributes: ['id', 'name', 'factStartDate', 'factFinishDate', 'statusId', 'allottedTime',
+          attributes: ['id', 'name', 'statusId', 'factStartDate', 'factFinishDate', 'allottedTime', 'createdAt', 'deletedAt',
+            'projectId', 'authorId', 'budget', 'riskBudget',
             [Sequelize.literal(`(SELECT sum(t.fact_execution_time)
                                 FROM tasks as t
                                 WHERE t.project_id = "Project"."id"
