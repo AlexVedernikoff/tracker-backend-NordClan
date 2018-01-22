cde64e6081c533ba51aa6dd36a4e10fcc7a72eea
Aleksandr Zotov <aleksandr.zotov@simbirsoft.com>
2018-01-22 16:42:43 +0400

    fix addition of the task in the player, when status changed

--- server/services/timesheets/utils/index.js
+++ server/services/timesheets/utils/index.js
@@ -7,6 +7,7 @@ exports.isNeedCreateDraft = async (task, statusId, onDate, currentUserId) => {
   }
 
   const queryParams = {
+    taskStatusId: statusId,
     taskId: task.id,
     onDate: new Date(onDate),
     userId: task.performerId
@@ -15,6 +16,13 @@ exports.isNeedCreateDraft = async (task, statusId, onDate, currentUserId) => {
   const timesheets = await queries.timesheet.all(queryParams);
   const drafts = await queries.timesheetDraft.all(queryParams);
 
+  const newStatus = task.statusId;
+  const previousStatus = task.taskStatus.id;
+
+  const checkChangePlayToStop = (prevSt, newSt) =>
+    (prevSt === 2 && newSt === 3) || (prevSt === 4 && newSt === 5) || (prevSt === 6 && newSt === 7);
+
   return (drafts.length === 0 && timesheets.length === 0)
-    && (models.TaskStatusesDictionary.CAN_CREATE_DRAFT_BY_CHANGES_TASKS_STATUS.indexOf(parseInt(statusId) > 0));
+    && !checkChangePlayToStop(previousStatus, newStatus)
+    && (models.TaskStatusesDictionary.CAN_CREATE_DRAFT_BY_CHANGES_TASKS_STATUS.indexOf(parseInt(statusId)) >= 0);
 };
