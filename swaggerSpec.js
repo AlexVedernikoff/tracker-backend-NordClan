9cdf0e4e87f61fb8ba4f888c441ae6ffd299ca21
Aleksandr Zotov <aleksandr.zotov@simbirsoft.com>
2018-01-25 15:47:15 +0400

    add user role updating and getting all users roles to the API description in the swagger spec

--- swaggerSpec.js
+++ swaggerSpec.js
@@ -1363,6 +1363,34 @@ module.exports = {
         ]
       }
     },
+    '/user/': {
+      put: {
+        tags: ['User'],
+        summary: 'Изменение глобальной роли пользователя',
+        parameters: [
+          {
+            name: 'id',
+            type: 'integer',
+            in: 'body',
+            required: true
+          },
+          {
+            name: 'globalRole',
+            type: 'string',
+            in: 'body',
+            required: true
+          }
+        ],
+        responses: responsesCodes
+      }
+    },
+    '/user/usersroles': {
+      get: {
+        tags: ['User'],
+        summary: 'Получение списка всех пользователей и их глобальных ролей',
+        responses: responsesCodes
+      }
+    },
 
 
     '/timesheetDraft/{timesheetDraftId}/': {
