78f2f74c097be06893b823cc49b28c0620b30ef4
Aleksandr Zotov <aleksandr.zotov@simbirsoft.com>
2018-02-26 15:22:02 +0400

    add aync await in calculate metrics controller

--- server/controllers/api/v1/MetricsController.js
+++ server/controllers/api/v1/MetricsController.js
@@ -14,16 +14,15 @@ exports.list = async (req, res, next) => {
     .catch(e => next(createError(e)));
 };
 
-exports.calculate = (req, res, next) => {
+exports.calculate = async (req, res, next) => {
   if (!req.params.projectId && !req.params.projectId.match(/^[0-9]+$/)) {
     return next(createError(400, 'projectId id must be int'));
   }
   try {
-    AgentService.calculateByProject(req.params.projectId).then(() => {
-      AgentService
+    await AgentService.calculateByProject(req.params.projectId).then(async () => {
+      await AgentService
         .list(req.body)
-        .then(metrics => res.json(metrics))
-        .catch(e => next(createError(e)));
+        .then(metrics => res.json(metrics));
     });
   } catch (error) {
     next(createError(error));
