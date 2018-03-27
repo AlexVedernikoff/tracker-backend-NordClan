d21aaf370582c0205a2e37c38a2bf84dc077a37f
Ivan <inferal73@gmail.com>
2018-03-27 17:07:46 +0400

    ST-1544 remove sending create action by sockets

--- server/controllers/api/v1/TaskController.js
+++ server/controllers/api/v1/TaskController.js
@@ -28,7 +28,6 @@ exports.create = async function (req, res, next) {
 
   try {
     const task = await TasksService.create(req.body);
-    TasksChannel.sendAction('create', task, res.io, task.projectId);
     if (task.performerId) await userSubscriptionEvents(models.ProjectEventsDictionary.values[1].id, { taskId: task.id });
     await userSubscriptionEvents(models.ProjectEventsDictionary.values[0].id, { taskId: task.id });
     res.json(task);
