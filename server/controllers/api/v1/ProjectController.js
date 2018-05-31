5f9217810ee74a9e8f7f26823a6d1fab2938bf56
Andrew Yudin <andrew.yudin@simbirsoft.com>
2018-05-31 14:29:00 +0400

    gitlab projects response fix

--- server/controllers/api/v1/ProjectController.js
+++ server/controllers/api/v1/ProjectController.js
@@ -258,11 +258,9 @@ exports.update = function (req, res, next){
                 const updatedParams = { ...req.body, id: model.id };
                 ProjectsChannel.sendAction('update', updatedParams, res.io, model.id);
 
-                if (req.body.gitlabProjectIds && req.body.gitlabProjectIds.length) {
+                if (req.body.gitlabProjectIds) {
                   gitlabProjectsOld = await gitLabService.projects.getProjects(gitlabProjectIdsOld);
                   model.dataValues.gitlabProjects = [...gitlabProjectsOld, ...gitlabProjectsNew];
-                } else {
-                  model.dataValues.gitlabProjects = [];
                 }
 
                 res.json(model);
