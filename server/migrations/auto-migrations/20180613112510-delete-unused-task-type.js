bf062b74171a4f1264d8ea476509b5599a7d4fb7
petyankin.av <aleksandr.petyankin@simbirsoft.com>
2018-06-13 11:58:49 +0300

    ST-2240 Add delete-unused-task-type migration

---
+++ server/migrations/auto-migrations/20180613112510-delete-unused-task-type.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: function (queryInterface) {
+    queryInterface.sequelize.query('UPDATE tasks SET "type_id" = 2, "is_task_by_client" = true WHERE "type_id" = 5;');
+    queryInterface.sequelize.query('DELETE FROM task_types WHERE "id" = 5');
+  },
+
+  down: function (queryInterface) {
+    queryInterface.sequelize.query('INSERT INTO task_types (id, name) VALUES (5, \'Баг от клиента\');');
+    queryInterface.sequelize.query('UPDATE tasks SET "type_id" = 5, "is_task_by_client" = false WHERE "type_id" = 2;');
+  }
+};
