cb038cb0c2bafe39a01602f621dd3e72d048a3e9
petyankin.av <aleksandr.petyankin@simbirsoft.com>
2018-06-14 13:54:50 +0300

    ST-2242 Added qaPlannedTime property to task

--- server/services/tasks/read/index.js
+++ server/services/tasks/read/index.js
@@ -1,5 +1,9 @@
 const taskRequests = require('./request');
 const createError = require('http-errors');
+const models = require('../../../models');
+const { getTaskFactTimeByQa } = require('../../../services/timesheets/spent');
+const { Sprint, Project } = models;
+
 
 exports.read = async (id, user) => {
   const task = await taskRequests.findByPrimary(id, user.dataValues.globalRole);
@@ -15,6 +19,26 @@ exports.read = async (id, user) => {
   if (task.tags) {
     task.tags = Object.keys(task.tags).map((k) => task.tags[k].name);
   }
+
+  let qaPercent;
+
+  if (task.sprintId) {
+    const sprint = await Sprint.findByPrimary(task.sprintId, {attributes: ['qaPercent']});
+    qaPercent = sprint.qaPercent;
+  }
+
+  if (!qaPercent) {
+    const project = await Project.findByPrimary(task.projectId, { attributes: ['qaPercent'] });
+    qaPercent = project ? project.qaPercent : null;
+  }
+
+  if (qaPercent && task.plannedExecutionTime) {
+    task.dataValues.qaPlannedTime = qaPercent * task.plannedExecutionTime * 0.01;
+  } else {
+    throw createError(500, 'QA planned time calculate error');
+  }
+
+  task.dataValues.qaFactExecutionTime = await getTaskFactTimeByQa(task.id);
 
   return task;
 };
