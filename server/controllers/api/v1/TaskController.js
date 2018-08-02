84123108132cd74bf603afe16e8cf96eacdbf01e
Roman Fazulianov <roman.fazulianov@simbirsoft.com>
2018-08-02 12:13:44 +0400

    Update TaskController.js

--- server/controllers/api/v1/TaskController.js
+++ server/controllers/api/v1/TaskController.js
@@ -145,8 +145,7 @@ exports.list = function (req, res, next) {
     return next(createError(400, 'pageSize must be int'));
   }
 
-  if (req.query.performerId
-    && !(Array.isArray(req.query.performerId) && req.query.performerId.every(performerId => parseInt(performerId) > 0))
+  if (!Array.isArray(req.query.performerId) || req.query.performerId.some(performerId => parseInt(performerId) < 1))
   ) {
     return next(createError(400, 'performerId must be array of numbers'));
   }
