781f5f29ae9ff51236d38be11ebdb78a4b6fa545
petyankin.av <aleksandr.petyankin@simbirsoft.com>
2018-08-09 16:58:23 +0300

    ST-6282 Fix sprint property

--- server/models/queries/timesheetDraft.js
+++ server/models/queries/timesheetDraft.js
@@ -83,6 +83,12 @@ exports.all = async function (conditions) {
             required: false,
             attributes: ['id', 'name'],
             paranoid: false
+          },
+          {
+            as: 'sprint',
+            model: models.Sprint,
+            required: true,
+            attributes: ['name']
           }
         ]
       },
