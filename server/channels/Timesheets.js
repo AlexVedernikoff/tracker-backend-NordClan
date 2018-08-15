a9cb12179b643261eda912ff869e864a7fb0be0a
petyankin.av <aleksandr.petyankin@simbirsoft.com>
2018-08-15 22:20:29 +0300

    ST-3591 Add security for websocket

--- server/channels/Timesheets.js
+++ server/channels/Timesheets.js
@@ -1,6 +1,15 @@
+const timesheets = require('./../models/queries/timesheet');
+
 exports.sendAction = (type, data, socketIO, userId) => {
   const action = getAction(type, data);
   emit(socketIO, action, userId);
+};
+
+exports.sendTaskUpdate = async (data, socketIO) => {
+  const action = getAction('update', data);
+  const userIds = await timesheets.all({taskId: { $eq: data.taskId }})
+    .then(sheets => sheets.map(sheet => sheet.id));
+  userIds.forEach(id => emit(socketIO, action, id));
 };
 
 function getAction (type, data) {
