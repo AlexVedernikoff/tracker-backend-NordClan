3c1f7b00a38e1bd60d885f622ada4c00f819ae1b
petyankin.av <aleksandr.petyankin@simbirsoft.com>
2018-08-20 17:39:33 +0300

    ST-3447 Fix metrics for tasks from backlog

--- server/services/agent/calculate/index.js
+++ server/services/agent/calculate/index.js
@@ -106,7 +106,7 @@ async function getMetrics (projectId){
     where: {
       sprintId: null
     },
-    attributes: ['id', 'sprintId', 'projectId'],
+    attributes: ['id', 'sprintId', 'projectId', 'typeId', 'statusId', 'isTaskByClient'],
     include: [
       {
         as: 'timesheets',
@@ -116,8 +116,7 @@ async function getMetrics (projectId){
       }
     ]
   }).then(tasks => tasks
-    .map(task => task.get({ 'plain': true }))
-    .filter(task => task.timesheets.length > 0));
+    .map(task => task.get({ 'plain': true })));
 
   const otherTimeSheets = await Timesheet.findAll({
     where: {
