8ce351d2fd4bffcd6f38c93553ca539c863b9d68
Evgeniy Kapnulin <evgeniy.kapnulin@simbirsoft.com>
2018-09-24 14:32:47 +0400

    major changes on dump process and dockerize postgres

---
+++ pg_db/pg_dump_dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+timestamp=$(date +%Y-%m-%d_%H:%M:%S)
+
+PGPASSWORD='la4eeH2m' pg_dump --username='simtrack' --host='simtrack-dev.simbirsoft' --port=33233 --format=p --file=$(pwd)/pg_db/dump_${timestamp}\.sql 'simtrack'
+rm -rf $(pwd)/pg_db/dumps/simtrack\.sql\.old ||:
+mv $(pwd)/pg_db/dumps/simtrack\.sql $(pwd)/pg_db/dumps/simtrack\.sql\.old
+cp $(pwd)/pg_db/dump_${timestamp}\.sql $(pwd)/pg_db/dumps/simtrack\.sql
