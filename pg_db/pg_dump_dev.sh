2a6626f6090d52045a1de093e5c41b21cbcc7043
Roman Fazulianov <roman.fazulianov@simbirsoft.com>
2018-09-24 14:59:34 +0400

    fix of sh

--- pg_db/pg_dump_dev.sh
+++ pg_db/pg_dump_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 timestamp=$(date +%Y-%m-%d_%H:%M:%S)
 
-PGPASSWORD='la4eeH2m' pg_dump --username='simtrack' --host='simtrack-dev.simbirsoft' --port=33233 --format=p --file=$(pwd)/pg_db/dump_${timestamp}\.sql 'simtrack'
-rm -rf $(pwd)/pg_db/dumps/simtrack\.sql\.old ||:
-mv $(pwd)/pg_db/dumps/simtrack\.sql $(pwd)/pg_db/dumps/simtrack\.sql\.old
-cp $(pwd)/pg_db/dump_${timestamp}\.sql $(pwd)/pg_db/dumps/simtrack\.sql
+PGPASSWORD='la4eeH2m' pg_dump --username='simtrack' --host='simtrack-dev.simbirsoft' --port=33233 --format=p --file=$(pwd)/pg_db/dump_${timestamp}_dev\.sql 'simtrack'
+rm -rf $(pwd)/pg_db/simtrack\.sql\.old ||:
+mv $(pwd)/pg_db/dumps/simtrack\.sql $(pwd)/pg_db/simtrack\.sql\.old
+cp $(pwd)/pg_db/dump_${timestamp}_dev\.sql $(pwd)/pg_db/dumps/simtrack\.sql
