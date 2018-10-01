36ef3b8f74128e389334745024b3aaf08b6701d6
ilya.gofshteyn <ilya.gofshteyn@simbirsoft.com>
2018-10-01 13:06:21 +0300

    ST-3312 fixed type bug

--- server/services/comment/unlinkAttachment.js
+++ server/services/comment/unlinkAttachment.js
@@ -4,10 +4,12 @@ const models = require('../../models');
 module.exports = async (taskId, attachmentId, next) => {
   const comments = await models.Comment.findAll({where: { taskId }});
   const promises = [];
+  const id = parseInt(attachmentId);
   try {
     comments.forEach(comment => {
-      if (comment.attachmentIds && comment.attachmentIds.indexOf(attachmentId) !== -1) {
-        let ids = comment.attachmentIds.filter(i => i !== attachmentId);
+      console.log('comment', comment);
+      if (comment.attachmentIds && comment.attachmentIds.indexOf(id) !== -1) {
+        let ids = comment.attachmentIds.filter(i => i !== id);
         ids = ids.length ? JSON.stringify(ids) : null;
         promises.push(comment.update({attachmentIds: ids}));
       }
