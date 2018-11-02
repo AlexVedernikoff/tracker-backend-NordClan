db5949ed5bdec93b05bbeac1e4572b6039b7eef6
petyankin.av <aleksandr.petyankin@simbirsoft.com>
2018-11-02 16:00:56 +0300

    ST-9589 Fix migration

--- server/migrations/auto-migrations/20181024141300-add-is_dev_ops-column.js
+++ server/migrations/auto-migrations/20181024141300-add-is_dev_ops-column.js
@@ -1,29 +1,20 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
 
-    return Promise.resolve()
-      .then(() => Promise.all([
-        queryInterface.addColumn(
-          'tasks',
-          'is_dev_ops',
-          {
-            type: Sequelize.BOOLEAN
-          }
-        )
-      ]))
-      .then(() => Promise.all([
-        queryInterface.sequelize.query(
-          'UPDATE tasks SET "is_dev_ops" = false;'
-        )
-      ]));
+    return queryInterface.addColumn(
+      'tasks',
+      'is_dev_ops',
+      {
+        type: Sequelize.BOOLEAN
+      }
+    ).then(() => queryInterface.sequelize.query(
+      'UPDATE tasks SET "is_dev_ops" = false;'
+    ));
   },
   down: function (queryInterface) {
-    return Promise.resolve()
-      .then(() => Promise.all([
-        queryInterface.removeColumn(
-          'tasks',
-          'is_dev_ops'
-        )
-      ]));
+    return queryInterface.removeColumn(
+      'tasks',
+      'is_dev_ops'
+    );
   }
 };
