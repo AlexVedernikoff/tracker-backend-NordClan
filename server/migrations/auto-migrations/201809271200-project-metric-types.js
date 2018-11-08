74e598ad659bfe8575750107182b7329089bbbd9
Chapurin <chapurin.vladimir.v@yandex.ru>
2018-11-08 14:29:35 +0400

    ST-6632. Edit description on migration

--- server/migrations/auto-migrations/201809271200-project-metric-types.js
+++ server/migrations/auto-migrations/201809271200-project-metric-types.js
@@ -1,7 +1,7 @@
 module.exports = {
   up: queryInterface => {
     return queryInterface.bulkInsert('metric_types', [
-      { id: 61, name: 'Метрика по комманде. UserId, кол-во выполненных задач,, кол-во пофикшенных багов, кол-во возвратов, кол-во связанных багов' }
+      { id: 61, name: 'Метрика по комманде. UserId, кол-во выполненных задач, кол-во возвратов задач, кол-во пофикшенных багов, кол-во возвратов багов, кол-во связанных багов' }
     ])
       .then(() => {
         return queryInterface.sequelize.query('ALTER TABLE metrics ALTER COLUMN "value" TYPE text');
