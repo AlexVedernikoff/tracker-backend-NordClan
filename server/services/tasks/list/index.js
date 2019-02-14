657568a80be2d3c4aeffd00a15837ab02242b335
Aleksei Gladkiy <aleksei.gladkiy@simbirsoft.com>
2019-02-14 21:37:28 +0400

    feature/ST-16817 Add task status to linked task tooltip

--- server/services/tasks/list/index.js
+++ server/services/tasks/list/index.js
@@ -7,14 +7,14 @@ const { Task, Tag, ItemTag } = models;
 const layoutAgnostic = require('../../layoutAgnostic');
 const { NOTAG } = require('../../../components/utils');
 
-function getTagsByTaskList(tasks) {
+function getTagsByTaskList (tasks) {
   const allTags = _.unionBy(...tasks.map(task => task.tags.map(o => o.dataValues)), o => o.name).map(tag => ({
     name: tag.name
   }));
   return allTags;
 }
 
-exports.list = async function(req) {
+exports.list = async function (req) {
   let prefixNeed = false;
 
   if (req.query.fields) {
@@ -66,11 +66,11 @@ exports.list = async function(req) {
     where: queryWhere,
     subQuery: false,
     order: models.sequelize.literal(
-      'CASE WHEN "sprint"."fact_start_date" <= now() AND "sprint"."fact_finish_date" >= now() THEN 1 ELSE 2 END' +
-        ', "sprint"."fact_start_date" ASC' +
-        ', "statusId" ASC' +
-        ', "prioritiesId" ASC' +
-        ', "name" ASC'
+      'CASE WHEN "sprint"."fact_start_date" <= now() AND "sprint"."fact_finish_date" >= now() THEN 1 ELSE 2 END'
+        + ', "sprint"."fact_start_date" ASC'
+        + ', "statusId" ASC'
+        + ', "prioritiesId" ASC'
+        + ', "name" ASC'
     )
   });
 
@@ -109,7 +109,7 @@ exports.list = async function(req) {
   return responseObject;
 };
 
-function createWhereForRequest(req, selectWithoutTags) {
+function createWhereForRequest (req, selectWithoutTags) {
   const where = {
     deletedAt: { $eq: null } // IS NULL
   };
@@ -276,14 +276,14 @@ function createWhereForRequest(req, selectWithoutTags) {
   return where;
 }
 
-async function createIncludeForRequest(tagsParams, prefixNeed, performerId, role, selectWithoutTags) {
+async function createIncludeForRequest (tagsParams, prefixNeed, performerId, role, selectWithoutTags) {
   const parsedTags = tagsParams
     ? tagsParams.map(el =>
-        el
-          .toString()
-          .trim()
-          .toLowerCase()
-      )
+      el
+        .toString()
+        .trim()
+        .toLowerCase()
+    )
     : null;
 
   const includeAuthor = {
@@ -319,7 +319,7 @@ async function createIncludeForRequest(tagsParams, prefixNeed, performerId, role
   const includeLLnkedTasks = {
     as: 'linkedTasks',
     model: models.Task,
-    attributes: ['id', 'name'],
+    attributes: ['id', 'name', 'statusId'],
     through: {
       model: models.TaskTasks,
       attributes: []
