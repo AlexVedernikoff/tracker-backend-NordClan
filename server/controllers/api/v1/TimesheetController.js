3770163bf97a59403636e2374760bad2b222ffaf
Alex Voronetskiy <alexandr.voronetskiy@gmail.com>
2019-10-29 15:14:02 +0300

    filter project ts via projectId

--- server/controllers/api/v1/TimesheetController.js
+++ server/controllers/api/v1/TimesheetController.js
@@ -4,9 +4,7 @@ const TasksService = require('../../../services/tasks');
 const TimesheetsChannel = require('../../../channels/Timesheets');
 const TaskChannel = require('../../../channels/Tasks');
 const models = require('../../../models');
-const {
-  getAverageNumberOfEmployees: _getAverageNumberOfEmployees
-} = require('../../../services/reports/utils');
+const { getAverageNumberOfEmployees: _getAverageNumberOfEmployees } = require('../../../services/reports/utils');
 
 exports.create = async (req, res, next) => {
   const userId = req.body.userId || req.user.id; // Todo: validate user rights
@@ -21,18 +19,16 @@ exports.create = async (req, res, next) => {
     return next(createError(400, 'spentTime wrong'));
   }
 
-  TimesheetService
-    .create(timesheetParams)
+  TimesheetService.create(timesheetParams)
     .then(createdTimesheet => {
       TimesheetsChannel.sendAction('create', createdTimesheet, res.io, userId);
       res.json(createdTimesheet);
       if (taskId) {
-        TasksService
-          .read(taskId, req.user).then(task => {
-            if (task) {
-              TaskChannel.sendAction('update', task, res.io, task.projectId);
-            }
-          });
+        TasksService.read(taskId, req.user).then(task => {
+          if (task) {
+            TaskChannel.sendAction('update', task, res.io, task.projectId);
+          }
+        });
       }
     })
     .catch(e => {
@@ -44,12 +40,9 @@ exports.getTracksAll = async (req, res, next) => {
   const startDate = req.query.startDate;
   const endDate = req.query.endDate;
   const activeTasks = await TasksService.getActiveTasks(req.user.id);
-  const activeTask = activeTasks.length !== 0
-    ? activeTasks[0]
-    : await TasksService.getLastActiveTask(req.user.id);
+  const activeTask = activeTasks.length !== 0 ? activeTasks[0] : await TasksService.getLastActiveTask(req.user.id);
 
-  TimesheetService
-    .getTracksAll(startDate, endDate, req.user.id)
+  TimesheetService.getTracksAll(startDate, endDate, req.user.id)
     .then(tracks => {
       TimesheetsChannel.sendAction('setActiveTask', activeTask, res.io, req.user.id);
       res.json(tracks);
@@ -68,13 +61,9 @@ exports.getAverageNumberOfEmployees = async function (req, res, next) {
 
   const { dateBegin, dateEnd } = req.query;
 
-  const averageNumberOfEmployees = await _getAverageNumberOfEmployees(
-    dateBegin,
-    dateEnd,
-    {
-      precision: 1
-    }
-  );
+  const averageNumberOfEmployees = await _getAverageNumberOfEmployees(dateBegin, dateEnd, {
+    precision: 1
+  });
 
   res.json({ total: averageNumberOfEmployees });
 };
@@ -99,7 +88,6 @@ exports.list = async function (req, res, next) {
     req.checkQuery('taskId', 'taskId is invalid or not defined. You should define int taskId or time period').isInt();
   }
 
-
   const validationResult = await req.getValidationResult();
   if (!validationResult.isEmpty()) {
     return next(createError(400, validationResult));
@@ -109,16 +97,15 @@ exports.list = async function (req, res, next) {
   const dateEnd = req.query.dateEnd;
   const taskId = req.query.taskId;
   const userPSId = req.query.userPSId ? req.query.userPSId : null;
-  const task = taskId ? await models.Task.findByPrimary(taskId, {
-    attributes: ['id', 'projectId']
-  }) : null;
+  const task = taskId
+    ? await models.Task.findByPrimary(taskId, {
+      attributes: ['id', 'projectId']
+    })
+    : null;
   const isAllTimeSheets = taskId ? req.user.isAdminOfProject(task.projectId) : false;
-  const userId
-    = req.isSystemUser ? req.query.userId
-      : isAllTimeSheets ? false : req.user.id;
+  const userId = req.isSystemUser ? req.query.userId : isAllTimeSheets ? false : req.user.id;
 
-  TimesheetService
-    .list(dateBegin, dateEnd, taskId, userId, userPSId, req.isSystemUser)
+  TimesheetService.list(dateBegin, dateEnd, taskId, userId, userPSId, req.isSystemUser)
     .then(timesheets => res.json(timesheets))
     .catch(error => next(createError(error)));
 };
@@ -136,8 +123,7 @@ exports.listProject = async function (req, res, next) {
   const dateEnd = req.query.dateEnd;
   const projectId = req.params.projectId;
 
-  TimesheetService
-    .listProject(dateBegin, dateEnd, projectId, req.isSystemUser)
+  TimesheetService.listProject(dateBegin, dateEnd, projectId, req.isSystemUser)
     .then(timesheets => res.json(timesheets))
     .catch(error => next(createError(error)));
 };
@@ -153,29 +139,27 @@ exports.listAllProjects = async function (req, res, next) {
 
   const dateBegin = req.query.dateBegin;
   const dateEnd = req.query.dateEnd;
+  const projectId = req.query.projectId;
 
-  TimesheetService
-    .listProject(dateBegin, dateEnd, undefined, req.isSystemUser)
+  TimesheetService.listProject(dateBegin, dateEnd, projectId, req.isSystemUser)
     .then(timesheets => {
       res.json(timesheets);
     })
     .catch(error => next(createError(error)));
 };
 
-
 const updateTimesheet = async (req, res) => {
-  const {taskId} = req.body;//eslint-disable-line
-  await TimesheetService
-    .update(req)
-    .then(sheet => Promise.all([
-      taskId && TasksService
-        .read(taskId, req.user),
-      sheet,
-      taskId && TasksService
-        .read(sheet.taskId, req.user)
-    ]))
+  const { taskId } = req.body; //eslint-disable-line
+  await TimesheetService.update(req)
+    .then(sheet =>
+      Promise.all([
+        taskId && TasksService.read(taskId, req.user),
+        sheet,
+        taskId && TasksService.read(sheet.taskId, req.user)
+      ])
+    )
     .then(([task, sheet, taskSheet]) => {
-      TimesheetsChannel.sendAction('update', !task ? sheet : {...sheet, task }, res.io, sheet.userId);
+      TimesheetsChannel.sendAction('update', !task ? sheet : { ...sheet, task }, res.io, sheet.userId);
       if (task) {
         TaskChannel.sendAction('update', taskSheet, res.io, taskSheet.projectId);
       }
@@ -183,13 +167,11 @@ const updateTimesheet = async (req, res) => {
 };
 
 exports.update = async (req, res, next) => {
-
   if (req.body.spentTime && req.body.spentTime < 0) {
     return next(createError(400, 'spentTime wrong'));
   }
   if (Array.isArray(req.body.sheetId)) {
     const requests = req.body.sheetId.map(id => {
-
       const singleReq = {
         ...req,
         body: {
@@ -199,14 +181,20 @@ exports.update = async (req, res, next) => {
       };
       return updateTimesheet(singleReq, res);
     });
-    Promise.all(requests).then(res.end()).catch(e => next(createError(e)));
+    Promise.all(requests)
+      .then(res.end())
+      .catch(e => next(createError(e)));
   } else {
-    updateTimesheet(req, res).then(res.end()).catch(e => next(createError(e)));
+    updateTimesheet(req, res)
+      .then(res.end())
+      .catch(e => next(createError(e)));
   }
 };
 
 exports.delete = async (req, res, next) => {
-  req.checkParams('timesheetId', 'timesheetId must be integer or comma-separated integers. Exp: 1,2,3').matches(/^\d+(,\d+)*$/);
+  req
+    .checkParams('timesheetId', 'timesheetId must be integer or comma-separated integers. Exp: 1,2,3')
+    .matches(/^\d+(,\d+)*$/);
   const validationResult = await req.getValidationResult();
   if (!validationResult.isEmpty()) {
     return next(createError(400, validationResult));
@@ -214,8 +202,7 @@ exports.delete = async (req, res, next) => {
 
   const timesheetIds = req.params.timesheetId.split(',');
 
-  TimesheetService
-    .destroy(timesheetIds, req.user.id)
+  TimesheetService.destroy(timesheetIds, req.user.id)
     .then(timesheets => {
       timesheets.forEach(deletedTimesheet => {
         TimesheetsChannel.sendAction('destroy', deletedTimesheet, res.io, req.user.id);
@@ -238,20 +225,20 @@ exports.updateDraft = async function (req, res, next) {
   }
 
   const draftId = req.params.sheetId || req.body.sheetId || req.query.sheetId;
-  const { sheetId, taskId, ...body} = req.body;//eslint-disable-line
+  const { sheetId, taskId, ...body } = req.body; //eslint-disable-line
 
-  TimesheetService
-    .updateDraft(body, draftId, req.user.id)
-    .then(sheet => Promise.all([
-      taskId && TasksService
-        .read(taskId, req.user),
-      sheet
-    ]))
-    .then(([task, {updatedDraft, createdTimesheet}]) => {
+  TimesheetService.updateDraft(body, draftId, req.user.id)
+    .then(sheet => Promise.all([taskId && TasksService.read(taskId, req.user), sheet]))
+    .then(([task, { updatedDraft, createdTimesheet }]) => {
       if (updatedDraft) {
-        TimesheetsChannel.sendAction('update', !task ? updatedDraft : {...updatedDraft, task}, res.io, req.user.id);
+        TimesheetsChannel.sendAction('update', !task ? updatedDraft : { ...updatedDraft, task }, res.io, req.user.id);
       } else if (createdTimesheet) {
-        TimesheetsChannel.sendAction('create', !task ? createdTimesheet : {...createdTimesheet, task}, res.io, req.user.id);
+        TimesheetsChannel.sendAction(
+          'create',
+          !task ? createdTimesheet : { ...createdTimesheet, task },
+          res.io,
+          req.user.id
+        );
       }
 
       res.end();
@@ -268,11 +255,15 @@ exports.submit = async function (req, res, next) {
     return next(createError(400, validationResult));
   }
 
-  return TimesheetService.submit(userId, req.body.dateBegin, req.body.dateEnd, req.body.projectId, req.body.justRejected)
+  return TimesheetService.submit(
+    userId,
+    req.body.dateBegin,
+    req.body.dateEnd,
+    req.body.projectId,
+    req.body.justRejected
+  )
     .then(result => {
-      result.forEach(
-        sheet => TimesheetsChannel.sendAction('update', sheet, res.io, userId)
-      );
+      result.forEach(sheet => TimesheetsChannel.sendAction('update', sheet, res.io, userId));
       res.json(result);
     })
     .catch(error => next(createError(400, error)));
@@ -291,9 +282,7 @@ exports.approve = async function (req, res, next) {
 
   return TimesheetService.approve(req.body.userId, req.body.dateBegin, req.body.dateEnd, req.body.projectId)
     .then(result => {
-      result.forEach(
-        sheet => TimesheetsChannel.sendAction('update', sheet, res.io, req.body.userId)
-      );
+      result.forEach(sheet => TimesheetsChannel.sendAction('update', sheet, res.io, req.body.userId));
       res.json(result);
     })
     .catch(error => next(createError(400, error)));
@@ -307,11 +296,10 @@ exports.reject = async function (req, res, next) {
   if (!validationResult.isEmpty()) {
     return next(createError(400, validationResult));
   }
+
   return TimesheetService.reject(req.body.userId, req.body.dateBegin, req.body.dateEnd, req.body.projectId)
     .then(result => {
-      result.forEach(
-        sheet => TimesheetsChannel.sendAction('update', sheet, res.io, req.body.userId)
-      );
+      result.forEach(sheet => TimesheetsChannel.sendAction('update', sheet, res.io, req.body.userId));
       res.json(result);
     })
     .catch(error => next(createError(400, error)));
