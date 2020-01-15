ee34c5336699e1c3762c5797c4bd340de6f240e5
Inna <inna.shutova@nordclan.com>
2020-01-15 12:53:44 +0400

    fix discussions

--- cronjobs/checkExternalUsersScript/checkExternalUsers.js
+++ cronjobs/checkExternalUsersScript/checkExternalUsers.js
@@ -2,71 +2,7 @@ const models = require('../../server/models');
 const emailService = require('../../server/services/email');
 const { email: { templateExternalUrl } } = require('../../server/configs');
 
-
-const sendMessage = async function (usr, str) {
-  const usersToProjectIds = await models.ProjectUsers.findAll({
-    where: {
-      userId: usr.id
-    },
-    attributes: [
-      'projectId'
-    ]
-  });
-  if (usersToProjectIds === null || usersToProjectIds.length === 0) {
-    return;
-  }
-  const listUsers = [];
-  for (const id of usersToProjectIds) {
-    const usrs = await models.ProjectUsers.findAll({
-      where: {
-        projectId: id.dataValues.projectId
-      }
-    });
-    if (usrs !== null
-            && usrs.length !== 0) {
-      usrs.forEach(a => {
-        listUsers.push(a.dataValues);
-      });
-
-    }
-  }
-  if (listUsers === null || listUsers.length === 0) {
-    return;
-  }
-  const leaderUsers = [];
-  for (const listUsersElement of listUsers) {
-    const findResult = await models.ProjectUsersRoles.findAll({
-      where: {
-        projectUserId: listUsersElement.id,
-        $or: [
-          {
-            projectRoleId: 1
-          },
-          {
-            projectRoleId: 2
-          }
-        ]
-      }
-    });
-    if (findResult !== null && !leaderUsers.includes(listUsersElement.userId)) {
-      leaderUsers.push(listUsersElement.userId);
-    }
-  }
-  const emails = [];
-  for (const leader of leaderUsers) {
-    const result = await models.User.findOne({
-      where: {
-        id: leader,
-        active: 1
-      }
-    });
-    if (result.emailPrimary !== null && !emails.includes(result.emailPrimary)) {
-      emails.push(result.dataValues.emailPrimary);
-    }
-  }
-  if (emails === null || emails.length === 0) {
-    return;
-  }
+const sendEmails = async function (usr, str, emails) {
   const template = emailService.template('checkExternalUser',
     {
       dayCount: str,
@@ -79,19 +15,137 @@ const sendMessage = async function (usr, str) {
   emails.forEach(email => {
     emailService.send({
       receiver: email,
+      // receiver: 'i.schutowa2011@yandex.ru',
       subject: template.subject,
       html: template.body
     });
   });
 };
 
+const getProjectsIds = async function (id) {
+  try {
+    const result = await models.ProjectUsers.findAll({
+      where: {
+        userId: id
+      },
+      attributes: [
+        'projectId'
+      ]
+    });
+    return result;
+  } catch (ex) {
+    return null;
+  }
+};
 
-exports.checkExternalUsers = async function () {
+const getUsersInProject = async function (ids) {
+  try {
+    const result = await models.ProjectUsers.findAll({
+      where: {
+        projectId: ids
+      }
+    });
+    return result;
+  } catch (ex) {
+    return null;
+  }
+};
+
+const getUsersProjectRoles = async function (ids) {
+  try {
+    const result = await models.ProjectUsersRoles.findAll({
+      where: {
+        projectUserId: ids,
+        $or: [
+          {
+            projectRoleId: 1
+          },
+          {
+            projectRoleId: 2
+          }
+        ]
+      }
+    });
+    return result;
+  } catch (ex) {
+    return null;
+  }
+};
+
+const getLeaderUsers = async function (ids) {
+  try {
+    const result = await models.User.findAll({
+      where: {
+        id: ids,
+        active: 1
+      }
+    });
+    return result;
+  } catch (ex) {
+    return null;
+  }
+};
+
+const sendMessage = async function (usr, str) {
+  const listUsers = [];
+  const listProjectUsers = [];
+  const projectIdsArray = [];
+  const leaderUsers = [];
+  const emails = [];
+
+  const usersToProjectIds = await getProjectsIds(usr.id);
+  if (usersToProjectIds === null || usersToProjectIds.length === 0) return;
+
+  usersToProjectIds.forEach(id => {
+    projectIdsArray.push(id.dataValues.projectId);
+  });
+
+  const usrs = await getUsersInProject(projectIdsArray);
+  if (usrs === null || usrs.length === 0) return;
+
+  usrs.forEach(a => {
+    listUsers.push(a.dataValues);
+    listProjectUsers.push(a.dataValues.id);
+  });
+  if (listProjectUsers.length === 0
+    || listUsers.length === 0) return;
+
+  const findResult = await getUsersProjectRoles(listProjectUsers);
+  if (findResult === null || usrs.length === 0) return;
+
+  findResult.forEach(result => {
+    const find = listUsers.find(a=>a.id === result.dataValues.projectUserId);
+    if (find !== null
+      && !leaderUsers.includes(find.userId)) {
+      leaderUsers.push(find.userId);
+    }
+  });
+
+  if (leaderUsers.length === 0) return;
+
+  const results = await getLeaderUsers(leaderUsers);
+  if (results === null || results.length === 0) return;
+
+  results.forEach(res => {
+    if (res.dataValues.emailPrimary !== null
+      && !emails.includes(res.dataValues.emailPrimary)) {
+      emails.push(res.dataValues.emailPrimary);
+    }
+  });
+
+  if (emails.length === 0) return;
+  await sendEmails(usr, str, emails);
+};
+
+
+module.exports.checkExternalUsers = async function () {
   const todayDateTime = new Date();
 
-  const today = new Date(todayDateTime.getFullYear(),
+  const today = new Date(
+    todayDateTime.getFullYear(),
     todayDateTime.getMonth(),
-    todayDateTime.getDate());
+    todayDateTime.getDate()
+  );
 
   const users = await models.User.findAll({
     where: {
@@ -103,8 +157,7 @@ exports.checkExternalUsers = async function () {
     }
   });
 
-
-  users.filter(usr => {
+  users.forEach(usr => {
     const dateTimeExpire = new Date(usr.expiredDate);
     const razn = Math.round((dateTimeExpire - today) / (1000 * 60 * 60 * 24));
 
@@ -114,7 +167,7 @@ exports.checkExternalUsers = async function () {
     if (razn === 3) {
       sendMessage(usr.dataValues, '3 дня');
     }
-    if (razn === 1) {
+    if (razn === 17) {
       sendMessage(usr.dataValues, 'день');
     }
   });
