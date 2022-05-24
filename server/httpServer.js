4bd05b624258de9b110813e94e6d446cb382c5c1
alex <alex.karamushko@nordclan.com>
2022-05-24 12:15:30 +0400

    TR-23392 - fix req.user set

--- server/httpServer.js
+++ server/httpServer.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const config = require('./configs');
 const { initApp } = require('./initApp');
+const getUserByToken = require('./middlewares/CheckTokenMiddleWare').getUserByToken;
 const { useJwtAuth } = require('./useJwtAuth');
 const app = express();
 
 const httpServer = require('http').Server(app);
 
+const io = require('socket.io')(httpServer, {
+  path: '/api/v1/socket',
+});
+
+io.sockets.on('connection', function (socket) {
+  getUserByToken(socket.handshake.headers)
+    .then(user => {
+      if (user) {
+        socket.join(`user_${user.dataValues.id}`);
+      }
+    })
+    .catch(err => {
+      console.error('Error when connection socket io. Reason: ', err);
+    });
+});
+
 exports.runHttpServer = function () {
+  app.use(function (req, res, next) {
+    res.io = io;
+    next();
+  });
+
   initApp(app);
 
   useJwtAuth(app, httpServer);
